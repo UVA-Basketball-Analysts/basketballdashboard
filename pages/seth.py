559a7b7f9ce08c079bb44a66423bafd771df1b0d
Hyun Suk (Max) Ryoo <hyunsukr@gmail.com>
2023-03-19 21:05:29 -0400

    update with seths code and google news

--- pages/seth.py
+++ pages/seth.py
@@ -2,15 +2,64 @@ import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import re
 
 from dash import html, dcc, callback, Input, Output
-# dash.register_page(__name__)
+
+
 dash.register_page(__name__, path='/seth')
 
-## dbc.container just puts things in the middle with some margins
+## Read in DF
+df = pd.read_csv("data/ClinicianReport.csv")
+
+df = df.sort_values(['meta__person__unique_id', 'timestamp'])
+
+## Define Layout
 def layout():
     return html.Div([
         dbc.Container([
-            html.H3("Seth's - PAGE")
+            html.H3("Seth's - PAGE"),
+            html.H1("Tales of Dunk and Leg"),
+            dcc.Dropdown(
+                id='playerid-dropdown',
+                options=[{'label': i, 'value': i} for i in df['meta__person__unique_id'].unique()],
+                value=df['meta__person__unique_id'].unique()[0]
+            ),       
+            html.H2("Hips"),
+            dcc.Graph(id='hips-graph'),
+            html.H2("Knees"),
+            #dcc.Graph(id='my-graph',figure=scatter),
+            html.H2("Ankles"),
+            #dcc.Graph(id='my-graph',figure=scatter)  
         ])
     ])
+@callback(
+    Output('hips-graph', 'figure'),
+    [Input(component_id='playerid-dropdown', component_property='value')]
+)
+
+def hips(playerid):
+    one_athlete = df[df['meta__person__unique_id'].str.contains(playerid)]
+    df_hips = one_athlete.filter(regex = re.compile(r'meta__person__unique_id|meta__session__session_guid|timestamp|hip|HIP|Hip'))
+    lines = []
+    for column in df_hips.columns[5:20]:
+        trace = go.Scatter(
+            x=df_hips['timestamp'],
+            y=df_hips[column],
+            mode='lines',
+            name=column
+        )
+        lines.append(trace)
+
+    layout = go.Layout(
+        title='Clinician Hips',
+        xaxis=dict(title='Timestamp'),
+        yaxis=dict(title='Value')
+    )
+    fig = go.Figure(data=lines, layout=layout)
+    return fig
+
