21e1df02d99a20f07645c085cd7475787c7a1448
Hyun Suk (Max) Ryoo <hyunsukr@gmail.com>
2023-03-21 18:12:53 -0400

    update with graphs

--- pages/workflow.py
+++ pages/workflow.py
@@ -1,11 +1,13 @@
 import dash
-from dash import html, dcc
+from dash import html, dcc,dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
-
+import numpy as np
 ## Import classes for visuals
 from utils.visuals import VisualsTool
 visualTool = VisualsTool()
+
+from collections import OrderedDict
 
 from dash import html, dcc, callback, Input, Output
 # dash.register_page(__name__)
@@ -15,14 +17,22 @@ df = pd.read_csv("data/ClinicianReport.csv")
 
 df = df.sort_values(['meta__person__unique_id', 'timestamp'])
 
+temp = pd.DataFrame(OrderedDict(
+    [
+        [
+            'Column {}'.format(i + 1), list(range(30))
+        ] for i in range(15)
+    ]
+))
 def layout():
     return html.Div([
         dbc.Container([
             html.H3("Trainer Workflow"),
             dcc.Dropdown(
                 id='playerid-dropdown',
-                options=[{'label': i, 'value': i} for i in df['meta__person__unique_id'].unique()],
-                value=df['meta__person__unique_id'].unique()[0]
+                options=[{'label': i, 'value': i} for i in np.concatenate([df['meta__person__unique_id'].unique(), ['all']])],
+                # value=df['meta__person__unique_id'].unique()[0]
+                value = 'all'
             ),
             dbc.Row(
                     [
@@ -40,7 +50,16 @@ def layout():
                                     html.H4("Difference squat"),
                                     dcc.Graph(id='difference-graph')
                                 ]),
-                                width=12)
+                                width=12),
+                        dbc.Col(html.Div([
+                                    html.H4("Loading Strategies"),
+                                    dcc.Graph(id='loading-strat-graph')
+                                ]),
+                                width=5),
+                        dbc.Col(html.Div([
+                                    html.H4("Loading Strategies Table"),
+                                    html.Div(id = 'loading-strat-table')]),
+                                width=7)
                     ]
                 ),
         ],fluid=True)
@@ -66,3 +85,35 @@ def get_unilateral(playerid):
 )
 def get_bivsuni(playerid):
     return visualTool.bivsuni(playerid)
+
+
+@callback(
+    Output('loading-strat-graph', 'figure'),
+    [Input(component_id='playerid-dropdown', component_property='value')]
+)
+def get_loading_strategy(playerid):
+    return visualTool.loading_strategy(playerid)
+
+@callback(
+    Output(component_id='loading-strat-table', component_property='children'),
+    [Input(component_id='playerid-dropdown', component_property='value')]
+)
+def loading_strat_table(playerid):
+    df_transposed = visualTool.loading_strategy_table(playerid) 
+    return html.Div(
+        [dash_table.DataTable(
+                                style_data={
+                                        'whiteSpace': 'normal',
+                                        'height': 'auto',
+                                    },
+                                data=df_transposed.to_dict('records'),
+                                columns=[{'name': i, 'id': i,} for i in df_transposed.columns],
+                                fixed_rows={'headers': True},
+                                style_cell={
+                                    'minWidth': 100, 'maxWidth': 100, 'width': 100
+                                },
+                                style_table={'overflowX': 'auto'},
+
+            )])
+                        
+    
--- utils/visuals.py
+++ utils/visuals.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import re
+from dash import Dash, dash_table
 
 class VisualsTool():
     def __init__(self):
@@ -34,7 +35,12 @@ class VisualsTool():
         ]
         
         bilateral_df = self.df[bilateral_columns]
-        player_df = bilateral_df[bilateral_df['meta__person__unique_id'].str.contains(playerid)]
+        
+        if playerid != 'all':
+            player_df = bilateral_df[bilateral_df['meta__person__unique_id'].str.contains(playerid)]
+        else:
+            player_df = bilateral_df
+            
         lines = []
         for column in player_df.columns:
             trace = go.Scatter(
@@ -81,7 +87,12 @@ class VisualsTool():
             "summary__unilateral_squat__loading_strategy__right_leg"
         ]
         unilateral_df = self.df[unilateral_columns]
-        player_df = unilateral_df[unilateral_df['meta__person__unique_id'].str.contains(playerid)]
+        
+        if playerid != 'all':
+            player_df = unilateral_df[unilateral_df['meta__person__unique_id'].str.contains(playerid)]
+        else:
+            player_df = unilateral_df
+            
         lines = []
         for column in player_df.columns:
             trace = go.Scatter(
@@ -151,7 +162,12 @@ class VisualsTool():
             matching_df[cols] = series
         flaten = [item for items in drop_columns for item in items]
         matching_df = matching_df.drop(flaten, axis=1)
-        player_df = matching_df[matching_df['meta__person__unique_id'].str.contains(playerid)]
+        
+        if playerid != 'all':
+            player_df = matching_df[matching_df['meta__person__unique_id'].str.contains(playerid)]
+        else:
+            player_df = matching_df
+            
         lines = []
         for column in player_df.columns:
             trace = go.Scatter(
@@ -163,10 +179,64 @@ class VisualsTool():
             lines.append(trace)
 
         layout = go.Layout(
-            title='Clinician Bilateral',
+            title='Clinician Difference',
             xaxis=dict(title='Timestamp'),
             yaxis=dict(title='Value')
         )
         fig = go.Figure(data=lines, layout=layout)
         # fig.update_traces(visible="legendonly")
         return fig
+    
+    def loading_strategy(self,playerid):
+        if playerid != 'all':
+            one_athlete = self.df[self.df['meta__person__unique_id'].str.contains(playerid)]
+        else:
+            one_athlete = self.df
+        df_strat = one_athlete.filter(regex = re.compile(r'timestamp|summary__bilateral_squat__loading_strategy__bilateral|summary__unilateral_squat__loading_strategy__left_leg|summary__unilateral_squat__loading_strategy__right_leg|summary__lateral_lunge__loading_strategy__left_leg|summary__lateral_lunge__loading_strategy__right_leg'))
+
+        lines = []
+        for column in df_strat.columns[2:7]:
+            trace = go.Scatter(x=df_strat['timestamp'],
+                               y=df_strat[column],
+                               name = column)
+            lines.append(trace)
+        layout = go.Layout(
+            title='Loading Strategies',
+            xaxis=dict(title='Timestamp'),
+            yaxis=dict(title='Strategy'),
+            # legend=dict(orientation="h")
+        )
+
+        fig = go.Figure(data=lines, layout=layout)
+        return fig
+    
+    def loading_strategy_table(self, playerid):
+
+        if playerid != 'all':
+            one_athlete = self.df[self.df['meta__person__unique_id'].str.contains(playerid)]
+        else:
+            one_athlete = self.df
+        df_strat = one_athlete.filter(regex = re.compile(r'timestamp|summary__bilateral_squat__loading_strategy__bilateral|summary__unilateral_squat__loading_strategy__left_leg|summary__unilateral_squat__loading_strategy__right_leg|summary__lateral_lunge__loading_strategy__left_leg|summary__lateral_lunge__loading_strategy__right_leg'))
+
+        df_strat = df_strat.drop('meta__session__session_timestamp', axis=1)
+
+
+        df_strat.set_index('timestamp', inplace=True)
+
+
+        df_transposed = df_strat.transpose()
+
+        df_transposed = df_transposed.reset_index()
+        df_transposed['strategies'] = df_transposed['index']
+
+        df_transposed = df_transposed.drop('index', axis=1)
+
+        strategies_col = df_transposed.pop('strategies')
+        df_transposed.insert(0, 'strategies', strategies_col)
+
+
+        df_transposed['strategies'] = df_transposed['strategies'].str.replace('_', ' ')
+
+        return df_transposed 
+
+        # return fig
